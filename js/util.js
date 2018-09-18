0a30a4b319e6186e350bf114c69d703c2808cec3
trannois <harold.trannois@u-picardie.fr>
2018-09-18 21:59:17 +0200

    Requête pour autoriser les notifications Notification.requestPermission()

--- js/util.js
+++ js/util.js
@@ -27,3 +27,26 @@ function registerServiceWorker() {
             console.error('Unable to register service worker.', err);
         });
 }
+
+/**
+ * Return une promise, ainsi la demande d'autorisation à l'utilisateur
+ * peut se faire en asynchrone
+ * @returns {Promise<any>}
+ */
+function askPermission() {
+    return new Promise(function (resolve, reject) {
+
+        Notification.requestPermission().then(function (result) {
+            //
+            if (result === 'denied') {
+                reject('La permission n\'a pas été validé, les notifications sont bloquées');
+            }
+            if (result === 'default') {
+                reject('La réponse à la requête pour la permission a été remis à plus tard.');
+            }
+            // Do something with the granted permission.
+            resolve();
+        })
+    })
+}
+
