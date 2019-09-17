3be1769f09ff0f5757760d1f8c382c5c1f2cc453
CARDON <cardonkevin02@gmail.com>
2019-09-17 17:01:45 +0200

    test

--- vapidKeys.js
+++ vapidKeys.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
     "subject": "mailto:cardonkevin02@gmail.com",
     "publicKey": "BNJXZfr87N4hBkd32pfC8VgNHQtigC9m0uIB7Xc4i8C2uEO0X-oQc1i6XcMX7YD88iRIMaTlYgTCbnXpMn90kyc",
     "privateKey": "l3JIEmYeWhHfkzkCJfUQ1-6YvV-x2xp-C1N3qj5OSZ4"
